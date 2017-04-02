d7c0e3e0c94d0d157ac0ddcfb8686a2d9c69a790
Caleb <caleb@eightbitorange.com>
2017-04-02 16:08:24 -0400

    Added gallery state functionality along with aphrodite conditional styling.

--- src/components/GalleryPreviewSelect.js
+++ src/components/GalleryPreviewSelect.js
@@ -20,19 +20,36 @@ class GalleryPreviewSelect extends Component {
     });
   }
 
- componentDidUpdate(){
-
- }
   render() {
 
     return (
-      <div onClick={this.onClick.bind(this)}>
+      <div className={css(this.props.image[2] === this.props.selected ? styles.red : styles.blue)} onClick={this.onClick.bind(this)}>
         <img src={this.props.image[0]} alt="MERCURIAL"/>
       </div>
     );
   }
 }
 
+const styles = StyleSheet.create({
+    red: {
+        backgroundColor: 'red'
+    },
 
+    blue: {
+        backgroundColor: 'blue'
+    },
+
+    hover: {
+        ':hover': {
+            backgroundColor: 'red'
+        }
+    },
+
+    small: {
+        '@media (max-width: 600px)': {
+            backgroundColor: 'red',
+        }
+    }
+});
 
 export default GalleryPreviewSelect;
