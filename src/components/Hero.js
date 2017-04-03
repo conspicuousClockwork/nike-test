60ff861a4aad0fe2ff67dad31659cc3d033425c1
Caleb <caleb@eightbitorange.com>
2017-04-03 05:03:41 -0400

    Added Aphrodite and flexbox styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import Logo from './Logo'
+import background from '../../assets/images/content-1-bg.png'
 class Hero extends Component {
   render() {
     return (
-      <div className="HeroContainer">
-        <div className="Hero">
-          <Logo />
+      <div className={css(styles.container)}>
+          <Logo/>
           <h1>{this.props.hero.title}</h1>
           <h2>{this.props.hero.text}</h2>
-        </div>
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      minHeight: '50vw',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      color: 'white',
+      alignItems: 'center',
+      padding: '3em',
+      display: 'flex',
+      marginBottom: '0em',
+      marginTop: '0em',
+      backgroundImage: 'url('+background+')',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '100%',
+      backgroundColor: '#27292B',
+      '@media (max-width: 1000px)': {
+        backgroundSize: 'auto'
+      }
+    },
+    innerContainer: {
+      backgroundSize: '100%',
+      color: 'white',
+      margin: '1em',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      display: 'flex',
+    },
+    text: {
+      marginBottom: '2em'
+    }
+});
 
 export default Hero;
