824e429bcba520944c0e810c732f213b2b3d3013
Caleb <caleb@eightbitorange.com>
2017-04-03 10:09:47 -0400

    Finished mobile flexbox styling and menu functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,28 +12,36 @@ class Header extends Component {
   }
 
   toggleMenu() {
-    this.state.open = !this.state.open;
+    this.setState({open: !this.state.open});
     console.log(this.state.open);
   }
 
+  componentDidUpdate(){
+    console.log('hello');
+  }
   render() {
     let navs;
     if (this.props.header.nav) {
       navs = this.props.header.nav.map(nav => {
-        return(<a href={nav.link}>{nav.label}</a>);
+        return(<a href={nav.link} className={css(styles.link)}>{nav.label}</a>);
       });
     }
     return (
-      <div className={css(styles.container)}>
-        <div className={css(styles.innerContainer)}>
-          <div className={css(styles.brand)}>
-            <Logo/>
+      <div>
+        <div className={css(styles.container)}>
+          <div className={css(styles.innerContainer)}>
+            <div className={css(styles.brand)}>
+              <Logo/>
+            </div>
+            <div className={css(styles.nav)}>
+              {navs}
+            </div>
           </div>
-          <div className={css(styles.nav)}>
-            {navs}
-          </div>
+          <SandwichMenu toggleMenu={this.toggleMenu.bind(this)} />
         </div>
-        <SandwichMenu toggleMenu={this.toggleMenu.bind(this)} />
+        <div className={css(this.state.open ? styles.mobileNav : styles.invisible)}>
+          {navs}
+        </div>
       </div>
     );
   }
@@ -70,6 +78,45 @@ const styles = StyleSheet.create({
       '@media (max-width: 1000px)': {
         display: 'none'
       }
+    },
+    mobileNav: {
+      transition: 'all 2s',
+      width: '100%',
+      height: 'auto',
+      padding: '2em',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+      color: '#A4D35A',
+      alignItems: 'center',
+      display: 'flex',
+      backgroundColor: '#27292B',
+      '@media (min-width: 1000px)': {
+        display: 'none'
+      }
+    },
+    invisible: {
+      transition: 'all 2s',
+      height: 0,
+      display: 'none'
+    },
+    link: {
+      margin: '1em',
+      color: 'white',
+      fontSize: '2em',
+      backgroundColor: 'black',
+      ':link': {
+          textDecoration: 'none'
+      },
+      ':visited': {
+          textDecoration: 'none'
+      },
+      ':hover': {
+          textDecoration: 'none'
+      },
+      ':active': {
+          textDecoration: 'none'
+      },
+      linkStyle: 'none'
     }
 });
 
