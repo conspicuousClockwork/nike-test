32666b9b6e82bac18b0c3f4b8c460bf4bad7554d
Caleb <caleb@eightbitorange.com>
2017-04-03 02:17:46 -0400

    Added img styling to Gallery Preview Select Component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import GalleryPreview from './GalleryPreview'
 import GalleryPreviewSelect from './GalleryPreviewSelect'
 import ThreeText from './ThreeText'
+import background from '../../assets/images/content-2-bg.png'
 
 class Gallery extends Component {
   constructor(){
@@ -22,12 +24,30 @@ class Gallery extends Component {
 
   render() {
     return (
-      <div className="GalleryContainer">
+      <div className={css(styles.container)}>
         <GalleryPreviewSelect selectImage={this.handleUpdatePreview.bind(this)} selected={this.state.selected} gallery={this.props.gallery}/>
         <GalleryPreview image={this.props.gallery.images[this.state.selected][1]} />
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      justifyContent: 'center',
+      display: 'flex',
+      backgroundImage: 'url('+background+')',
+      backgroundPosition: 'right',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain',
+      backgroundColor: '#27292B',
+      '@media (max-width: 1000px)': {
+        alignSelf: 'auto',
+        width: '75vw',
+        height: '75vw'
+      }
+    }
+});
 
 export default Gallery;
