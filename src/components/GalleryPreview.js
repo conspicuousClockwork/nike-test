ae9f5c0c7dfbc8767028706ba34dce1c968ffb60
Caleb <caleb@eightbitorange.com>
2017-04-03 02:01:34 -0400

    Added flexbox to Gallery Preview Component

--- src/components/GalleryPreview.js
+++ src/components/GalleryPreview.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import background from '../../assets/images/circle-bg.png';
 class GalleryPreviewSelect extends Component {
   render() {
     return (
-      <div className="GalleryPreview">
+      <div className={css(styles.container)}>
         <img src={this.props.image} className="PreviewImg" alt="MERCURIAL"/>
-        <img src={background} className="PreviewBackground" alt=""/>
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      alignSelf: 'flex-end',
+      display: 'flex',
+      alignContent: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '40vw',
+      height: '40vw',
+      backgroundImage: 'url(react/nike-test/assets/images/circle-bg.png)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '90%',
+      backgroundPosition: 'center',
+      '@media (max-width: 1000px)': {
+        alignSelf: 'auto',
+        width: '75vw',
+        height: '75vw'
+      }
+    }
+});
 
 export default GalleryPreviewSelect;
