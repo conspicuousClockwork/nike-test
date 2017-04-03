e30a6d1c7d2df4938ed5af3da874e9c17f82b31f
Caleb <caleb@eightbitorange.com>
2017-04-03 06:44:04 -0400

    Added Aphrodite and began optimizing header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,76 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import Logo from './Logo'
+import SandwichMenu from './SandwichMenu'
 class Header extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    }
+  }
+
+  toggleMenu() {
+    this.state.open = !this.state.open;
+    console.log(this.state.open);
+  }
 
   render() {
     let navs;
     if (this.props.header.nav) {
       navs = this.props.header.nav.map(nav => {
-        return(<li><a href={nav.link}>{nav.label}</a></li>);
+        return(<a href={nav.link}>{nav.label}</a>);
       });
     }
     return (
-      <nav className="navbar">
-        <div className="container-fluid">
-          <div className="brandContainer col-md-2">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#"><Logo /></a>
-            </div>
+      <div className={css(styles.container)}>
+        <div className={css(styles.innerContainer)}>
+          <div className={css(styles.brand)}>
+            <Logo/>
           </div>
-          <div>
-            <ul className="nav navbar-nav">
-              {navs}
-            </ul>
+          <div className={css(styles.nav)}>
+            {navs}
           </div>
         </div>
-      </nav>
-
+        <SandwichMenu toggleMenu={this.toggleMenu.bind(this)} />
+      </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      padding: 0,
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      color: '#A4D35A',
+      alignItems: 'center',
+      display: 'flex',
+      backgroundColor: '#27292B',
+      '@media (max-width: 1000px)': {
+      }
+    },
+    innerContainer: {
+      backgroundColor: '#999',
+      height: '100%',
+      color: 'white',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      display: 'flex',
+    },
+    brand: {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: '#A4D35A'
+    },
+    nav: {
+      '@media (max-width: 1000px)': {
+        display: 'none'
+      }
+    }
+});
 
 export default Header;
