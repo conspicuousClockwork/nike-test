a15fd3401b0f54cacc5248d5e445c7d1da51ff85
Caleb <caleb@eightbitorange.com>
2017-04-03 01:55:33 -0400

    Added flexbox styling to Gallery preview select component

--- src/components/GalleryPreviewSelect.js
+++ src/components/GalleryPreviewSelect.js
@@ -6,7 +6,7 @@ class GalleryPreviewSelect extends Component {
     super();
     this.state = {
       visible: false,
-      selected: []
+      selected: [0]
     }
   }
 
@@ -25,16 +25,16 @@ class GalleryPreviewSelect extends Component {
     let previewSelects;
     if (this.props.gallery.images) {
       previewSelects = this.props.gallery.images.map(image => {
-        return (<div className={css(image[2] === this.state.selected[0] ? styles.red : styles.blue)} onClick={this.onClick.bind(this,[image[2]])}><img src={image[0]}/></div>);
+        return (<div className={css(image[2] === this.state.selected[0] ? styles.active : styles.inactive,styles.choice)} onClick={this.onClick.bind(this,[image[2]])}><img src={image[0]}/></div>);
       });
     }
     return (
-      <div>
-        <div>
+      <div className={css(styles.container)}>
+        <div className={css(styles.text)}>
           <h4>{this.props.gallery.subTitle}</h4>
           <h3>{this.props.gallery.title}</h3>
         </div>
-        <div>
+        <div className={css(styles.choices)}>
           {previewSelects}
         </div>
       </div>
@@ -43,23 +43,57 @@ class GalleryPreviewSelect extends Component {
 }
 
 const styles = StyleSheet.create({
-    red: {
-        backgroundColor: 'red'
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      padding: '0px',
+      margin: '20px'
     },
 
-    blue: {
-        backgroundColor: 'blue'
+    text: {
+      color: 'white',
+      lineHeight: 0,
+      margin: '5px'
     },
 
-    hover: {
-        ':hover': {
-            backgroundColor: 'red'
-        }
+    choice: {
+      display: 'flex',
+      width: '100%',
+      padding: '5px',
+      marginTop: '10px',
+      marginLeft: '10px',
+      marginBottom: '10px',
+      marginRight: '0px',
+      alignContent: 'center',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      height: '5em',
+      '@media (max-width: 1000px)': {
+          alignItems: 'center'
+      }
     },
 
-    small: {
-        '@media (max-width: 600px)': {
-            backgroundColor: 'red',
+    active: {
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      borderColor: '#A4D35A',
+      borderRadius: '2px'
+    },
+
+    inactive: {
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      borderColor: 'transparent',
+      borderRadius: '2px'
+    },
+
+    choices: {
+        '@media (max-width: 1000px)': {
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          alignItems: 'center'
         }
     }
 });
