9884c0fd4b449a4defbe88209162f3b659a8fa11
Caleb <caleb@eightbitorange.com>
2017-04-03 03:27:43 -0400

    Added flexbox styling and aphrodite

--- src/components/ThreeText.js
+++ src/components/ThreeText.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TextBox from './TextBox'
-
+import { StyleSheet, css } from 'aphrodite';
 class ThreeText extends Component {
   render() {
     let textBoxes;
@@ -10,15 +10,33 @@ class ThreeText extends Component {
       });
     }
     return (
-      <div className="InfoContainer">
-        <div className="ThreeText container-fluid">
-          <div className="row">
-            {textBoxes}
-          </div>
+      <div className={css(styles.container)}>
+        <div className={css(styles.innerContainer)}>
+          {textBoxes}
         </div>
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      justifyContent: 'center',
+      marginTop: '3em',
+      marginBottom: '3em',
+      display: 'flex'
+    },
+    innerContainer: {
+      width: '75%',
+      justifyContent: 'space-between',
+      alignItems: 'flex-end',
+      display: 'flex',
+      '@media (max-width: 1000px)': {
+        flexDirection: 'column-reverse',
+        alignItems: 'center'
+      }
+    }
+});
 
 export default ThreeText;
